19d301e861f0e48c8e8c3b63cbebe94975ba3143
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-20 19:00:03 +0100

    added: Income dtos

---
+++ FinanceManagerApi/Models/Income/IncomeDto.cs
@@ -0,0 +1,16 @@
+﻿using FinanceManagerApi.Entities;
+
+namespace FinanceManagerApi.Models.Income
+{
+    public class IncomeDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public DateOnly Date { get; set; }
+        public decimal Amount { get; set; }
+        public int IncomeCategoryId { get; set; }
+        public string IncomeCategory { get; set; } = null!;
+        public int CurrencyId { get; set; }
+        public string Currency { get; set; } = null!;
+    }
+}
---
+++ FinanceManagerApi/Models/Income/UpdateRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceManagerApi.Models.Income
+{
+    public class UpdateRequestDto
+    {
+        public string? Title { get; set; }
+        public DateOnly? Date { get; set; }
+        public decimal? Amount { get; set; }
+        public int? IncomeCategoryId { get; set; }
+        public int? CurrencyId { get; set; }
+    }
+}
