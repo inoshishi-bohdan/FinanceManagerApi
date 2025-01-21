3b8bba88850c0bd82f22a155b422b9e2834a6d6b
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 16:55:02 +0100

    added: income category dto

---
+++ FinanceManagerApi/Models/IncomeCategory/IncomeCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceManagerApi.Models.IncomeCategory
+{
+    public class IncomeCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
