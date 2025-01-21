80244526740f8702150e3481025da0ef196316aa
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 17:02:36 +0100

    added: ExpenseCategoryController

---
+++ FinanceManagerApi/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using FinanceManagerApi.Data;
+using FinanceManagerApi.Extensions;
+using FinanceManagerApi.Models.ExpenseCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseCategoryController(FinanceManagerDbContext dbContext) : ControllerBase
+    {
+        [HttpGet("getList")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<ExpenseCategoryDto>>> GetAllExpenseCategories()
+        {
+            var response = await dbContext.ExpenseCategories.ToExpenseCategoryDtoListAsync();
+
+            return Ok(response);
+        }
+    }
+}
