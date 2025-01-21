7f8c0f533b96c727dd4917e23771b1ef6524d1f7
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 17:02:03 +0100

    added: expense category dto

---
+++ FinanceManagerApi/Models/ExpenseCategory/ExpenseCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceManagerApi.Models.ExpenseCategory
+{
+    public class ExpenseCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
