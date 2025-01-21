00d647bcfca6e47d7b62342cf61e6dbc163f550e
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 16:55:42 +0100

    added: IncomeCategoryController

---
+++ FinanceManagerApi/Controllers/IncomeCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using FinanceManagerApi.Data;
+using FinanceManagerApi.Extensions;
+using FinanceManagerApi.Models.IncomeCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncomeCategoryController(FinanceManagerDbContext dbContext) : ControllerBase
+    {
+        [HttpGet("getList")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<IncomeCategoryDto>>> GetAllIncomeCategories()
+        {
+            var response = await dbContext.IncomeCaregories.ToIncomeCategoryDtoListAsync();
+
+            return Ok(response);
+        }
+    }
+}
