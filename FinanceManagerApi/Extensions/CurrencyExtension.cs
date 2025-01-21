168e21289920ff2604959d533f61ac746265b503
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 14:21:33 +0100

    added: CurrencyExtension

---
+++ FinanceManagerApi/Extensions/CurrencyExtension.cs
@@ -0,0 +1,18 @@
+﻿using FinanceManagerApi.Entities;
+using FinanceManagerApi.Models.Currency;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceManagerApi.Extensions
+{
+    public static class CurrencyExtension
+    {
+        public static async Task<List<CurrencyDto>> ToCurrencyDtoListAsync(this IQueryable<Currency> currencies)
+        {
+            return await currencies.Select(currency => new CurrencyDto
+            {
+                Id = currency.Id,
+                Name = currency.Name,
+            }).ToListAsync();
+        }
+    }
+}
