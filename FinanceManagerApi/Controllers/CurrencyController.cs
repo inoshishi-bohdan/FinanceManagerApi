0b9d7e031eb313cadd6ef25584b6ad005ea65ac9
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 16:36:22 +0100

    added: CurrencyController

---
+++ FinanceManagerApi/Controllers/CurrencyController.cs
@@ -0,0 +1,21 @@
+﻿using FinanceManagerApi.Data;
+using FinanceManagerApi.Extensions;
+using FinanceManagerApi.Models.Currency;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController(FinanceManagerDbContext dbContext) : ControllerBase
+    {
+        [HttpGet("getList")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CurrencyDto>>> GetAllCurrencies()
+        {
+            var response = await dbContext.Currencies.ToCurrencyDtoListAsync();
+
+            return Ok(response);
+        }
+    }
+}
