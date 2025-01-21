4d26f03183e80781157206545d9c5e379718cc22
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 14:21:03 +0100

    added: currency dto

---
+++ FinanceManagerApi/Models/Currency/CurrencyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceManagerApi.Models.Currency
+{
+    public class CurrencyDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
