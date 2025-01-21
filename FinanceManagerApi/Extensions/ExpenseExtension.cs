5e27b3f0efcde8ef18e9c8dc5bd3d52d39fa6c1e
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-21 13:22:03 +0100

    added: ExpenseExtension

---
+++ FinanceManagerApi/Extensions/ExpenseExtension.cs
@@ -0,0 +1,39 @@
+﻿using FinanceManagerApi.Entities;
+using FinanceManagerApi.Models.Expense;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceManagerApi.Extensions
+{
+    public static class ExpenseExtension
+    {
+        public static async Task<List<ExpenseDto>> ToExpenseDtoListAsync(this IQueryable<Expense> expenses)
+        {
+            return await expenses.Select(expense => new ExpenseDto
+            {
+                Id = expense.Id,
+                Title = expense.Title,
+                Date = expense.Date,
+                Amount = expense.Amount,
+                Currency = expense.Currency.Name,
+                CurrencyId = expense.CurrencyId,
+                ExpenseCategory = expense.ExpenseCategory.Name,
+                ExpenseCategoryId = expense.ExpenseCategoryId,
+            }).ToListAsync();
+        }
+
+        public static ExpenseDto ToExpenseDto(this Expense entry)
+        {
+            return new ExpenseDto
+            {
+                Id = entry.Id,
+                Title = entry.Title,
+                Date = entry.Date,
+                Amount = entry.Amount,
+                CurrencyId = entry.CurrencyId,
+                Currency = entry.Currency.Name,
+                ExpenseCategoryId = entry.ExpenseCategoryId,
+                ExpenseCategory = entry.ExpenseCategory.Name
+            };
+        }
+    }
+}
