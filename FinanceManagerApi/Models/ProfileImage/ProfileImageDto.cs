82968a65180cbf640a02ded05c9d89dbbc353d5b
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-22 12:11:52 +0100

    added: profile image dto

---
+++ FinanceManagerApi/Models/ProfileImage/ProfileImageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceManagerApi.Models.ProfileImage
+{
+    public class ProfileImageDto
+    {
+        public int Id { get; set; }
+        public string Path { get; set; } = null!;
+        public string? Caption { get; set; }
+    }
+}
