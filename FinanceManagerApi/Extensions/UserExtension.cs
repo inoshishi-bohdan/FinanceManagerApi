9015846b9b50852ff5a6fc798cbc4489263191fe
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-22 16:53:24 +0100

    added: UserExtension

---
+++ FinanceManagerApi/Extensions/UserExtension.cs
@@ -0,0 +1,19 @@
+﻿using FinanceManagerApi.Entities;
+using FinanceManagerApi.Models.User;
+
+namespace FinanceManagerApi.Extensions
+{
+    public static class UserExtension
+    {
+        public static UserDto ToUserDto(this User entry)
+        {
+            return new UserDto
+            {
+                UserName = entry.UserName,
+                Email = entry.Email,
+                Password = entry.PasswordHash,
+                ProfileImageId = entry.ProfileImageId
+            };
+        }
+    }
+}
