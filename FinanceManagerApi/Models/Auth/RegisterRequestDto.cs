e3cb08c8ce3d434e437b84c685fd22c1ba779b98
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-22 16:51:54 +0100

    added: RegisterRequestDto

---
+++ FinanceManagerApi/Models/Auth/RegisterRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceManagerApi.Models.Auth
+{
+    public class RegisterRequestDto
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
