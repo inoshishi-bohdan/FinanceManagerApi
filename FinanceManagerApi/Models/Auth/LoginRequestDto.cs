0e8beb67c944700b189243cefd5eb1f7155bb00a
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-22 16:51:41 +0100

    added: LoginRequestDto

---
+++ FinanceManagerApi/Models/Auth/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceManagerApi.Models.Auth
+{
+    public class LoginRequestDto
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
