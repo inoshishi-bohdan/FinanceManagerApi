5de2b5b5376183175c10191e34bb94f27651b611
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-22 12:12:17 +0100

    added: ProfileImageController

---
+++ FinanceManagerApi/Controllers/ProfileImageController.cs
@@ -0,0 +1,21 @@
+﻿using FinanceManagerApi.Data;
+using FinanceManagerApi.Extensions;
+using FinanceManagerApi.Models.ProfileImage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileImageController(FinanceManagerDbContext dbContext) : ControllerBase
+    {
+        [HttpGet("getList")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<ProfileImageDto>>> GetAllProfileImages()
+        {
+            var response = await dbContext.ProfileImages.ToProfileImageDtoListAsync();
+
+            return Ok(response);
+        }
+    }
+}
