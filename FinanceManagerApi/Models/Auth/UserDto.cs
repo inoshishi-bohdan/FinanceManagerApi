7346ff9db8ec7289f89c5d62f4060c029f854ce0
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-22 16:53:12 +0100

    fix: moved UserDto

--- FinanceManagerApi/Models/Auth/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinanceManagerApi.Models.Auth
-{
-    public class UserDto
-    {
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
-    }
-}
---
+++ FinanceManagerApi/Models/User/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceManagerApi.Models.User
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int ProfileImageId { get; set; }
+    }
+}
