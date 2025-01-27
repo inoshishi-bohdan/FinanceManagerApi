0a1ed37867407d78d3ab2ff872f0b4dc9b08600f
Bohdan Koba <bohdan.koba@dxc.com>
2025-01-27 16:36:42 +0100

    added: ProfileImageController getMyProfileImage endpoint

--- FinanceManagerApi/Controllers/ProfileImageController.cs
+++ FinanceManagerApi/Controllers/ProfileImageController.cs
@@ -1,13 +1,16 @@
 ﻿using FinanceManagerApi.Data;
 using FinanceManagerApi.Extensions;
 using FinanceManagerApi.Models.ProfileImage;
+using FinanceManagerApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceManagerApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProfileImageController(FinanceManagerDbContext dbContext) : ControllerBase
+    public class ProfileImageController(FinanceManagerDbContext dbContext, IUserService userService) : ControllerBase
     {
         [HttpGet("getList")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -16,6 +19,38 @@ namespace FinanceManagerApi.Controllers
             var response = await dbContext.ProfileImages.ToProfileImageDtoListAsync();
 
             return Ok(response);
+        }
+
+        [HttpGet("getMyProfileImage")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize]
+        public async Task<ActionResult<ProfileImageDto>> GetMyProfileImage()
+        {
+            var myId = userService.GetMyId();
+
+            if (myId == null)
+            {
+                return Unauthorized("Couldn't get user id from http context.");
+            }
+
+            var user = await dbContext.Users.AsQueryable().FirstOrDefaultAsync(x => x.Id == myId);
+
+            //check if user record exists
+            if (user == null)
+            {
+                return NotFound($"User with ID {myId} was not found.");
+            }
+
+            var profileImage = await dbContext.ProfileImages.FindAsync(user.ProfileImageId);
+
+            if (profileImage == null) 
+            {
+                return NotFound($"Profile image with ID {user.ProfileImageId} was not found.");
+            }
+
+            return Ok(profileImage.ToProfileImageDto());
         }
     }
 }
