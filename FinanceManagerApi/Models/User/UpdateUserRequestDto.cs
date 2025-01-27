f3f8010cf48efd164ec339103638e017b688f61d
Bohdan Koba <kobabogdan78@gmail.com>
2025-01-27 11:41:03 +0100

    added: user dtos

---
+++ FinanceManagerApi/Models/User/UpdateUserRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceManagerApi.Models.User
+{
+    public class UpdateUserRequestDto
+    {
+        public string? UserName { get; set; } 
+        public string? Email { get; set; } 
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public int ProfileImageId { get; set; }
+    }
+}
