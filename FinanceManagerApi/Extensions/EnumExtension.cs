3cf998e6133491a6c9ce85fdab991ff23dfa12f9
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-23 15:50:24 +0100

    added: EnumExtension

---
+++ FinanceManagerApi/Extensions/EnumExtension.cs
@@ -0,0 +1,17 @@
+﻿using FinanceManager.Util;
+using System.Reflection;
+
+namespace FinanceManagerApi.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string GetDisplayAsOrName(this Enum value)
+        {
+            var members = value.GetType().GetMember(value.ToString());
+            if (members.Length == 0) return value.ToString();
+            var attribute = members.First().GetCustomAttribute<DisplayAsAttribute>();
+            if (attribute == null) return value.ToString();
+            return attribute.DisplayAs;
+        }
+    }
+}
