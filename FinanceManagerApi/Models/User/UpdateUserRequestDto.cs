5690ef0cbbb83263ac5e064772b083c37f70ff5a
Bohdan Koba <bohdan.koba@dxc.com>
2025-02-26 09:50:06 +0100

    fix: nullable ProfileImageId in update request

--- FinanceManagerApi/Models/User/UpdateUserRequestDto.cs
+++ FinanceManagerApi/Models/User/UpdateUserRequestDto.cs
@@ -6,6 +6,6 @@
         public string? Email { get; set; } 
         public string? OldPassword { get; set; }
         public string? NewPassword { get; set; }
-        public int ProfileImageId { get; set; }
+        public int? ProfileImageId { get; set; }
     }
 }
