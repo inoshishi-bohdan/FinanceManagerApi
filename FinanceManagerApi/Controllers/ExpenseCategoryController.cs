2eb7777aaababff27918213e1a02e66ba342c721
Bohdan Koba <bohdan.koba@dxc.com>
2025-03-21 16:18:58 +0100

    fix: remove ExpenseExtension

--- FinanceManagerApi/Controllers/ExpenseCategoryController.cs
+++ FinanceManagerApi/Controllers/ExpenseCategoryController.cs
@@ -1,19 +1,22 @@
-﻿using FinanceManagerApi.Data;
+﻿using AutoMapper;
+using FinanceManagerApi.Data;
 using FinanceManagerApi.Extensions;
 using FinanceManagerApi.Models.ExpenseCategory;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceManagerApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ExpenseCategoryController(FinanceManagerDbContext dbContext) : ControllerBase
+    public class ExpenseCategoryController(FinanceManagerDbContext dbContext, IMapper mapper) : ControllerBase
     {
         [HttpGet("getList")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<List<ExpenseCategoryDto>>> GetAllExpenseCategories()
         {
-            var response = await dbContext.ExpenseCategories.ToExpenseCategoryDtoListAsync();
+            var expenseCategories = await dbContext.ExpenseCategories.ToListAsync();
+            var response = mapper.Map<List<ExpenseCategoryDto>>(expenseCategories);
 
             return Ok(response);
         }
---
+++ FinanceManagerApi/Models/ExpenseCategory/ExpenseProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace FinanceManagerApi.Models.ExpenseCategory
+{
+    public class ExpenseProfile: Profile
+    {
+        public ExpenseProfile()
+        {
+            CreateMap<Entities.ExpenseCategory, ExpenseCategoryDto>();
+        }
+    }
+}
